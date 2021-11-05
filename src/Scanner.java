1ef4427302fa0e38ef97ed2b731eee907163ea32
wgulbin <wgulbin@genesisedu.com>
2021-11-05 12:20:57 -0400

    Scanner - support for string/number literals

--- src/Scanner.java
+++ src/Scanner.java
@@ -42,9 +42,99 @@ public class Scanner
             case ';' -> addToken(TokenType.SEMICOLON);
             case '*' -> addToken(TokenType.STAR);
 
-            default -> Lox.error(line, "Unexpected character.");
+            case '!' -> addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+            case '=' -> addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+            case '<' -> addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+            case '>' -> addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+
+            case ' ', '\t', '\r' -> {}
+
+            case '\n' -> line++;
+
+            case '"' -> string();
+
+
+            case '/' -> {
+                if (match('/'))
+                {
+                    // A comment goes until the end of the line
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                }
+                else
+                    addToken(TokenType.SLASH);
+            }
+
+            default -> {
+                if(isDigit(c))
+                    number();
+                else
+                    Lox.error(line, "Unexpected character.");
+            }
         }
     }
+
+    private boolean isDigit(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number()
+    {
+        while (isDigit(peek())) advance();
+
+        if(peek() == '.' && isDigit(peekNext()))
+        {
+            advance();
+            while(isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext()
+    {
+        if( current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private void string()
+    {
+        while (peek() != '"' && !isAtEnd())
+        {
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd())
+        {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance(); // Closing "
+
+        //Trim surrounding quotes
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+
+    private char peek()
+    {
+        if (isAtEnd()) return '\0';
+
+        return source.charAt(current);
+    }
+
+    private boolean match(char expected)
+    {
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+
+        //Expected does exist, we consume it and return true
+        current++;
+        return true;
+    }
+
     private char advance()
     {
         return source.charAt(current++);
